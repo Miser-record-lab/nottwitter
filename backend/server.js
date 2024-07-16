0089b0b2882889a3c594673e110dd9ce7f410658
“Hugo <miseryh.pro@gmail.com>
2024-07-16 09:24:34 +0200

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 console.log(process.env.MONGO_URI);
+
+app.use(express.json()); // to parse req.body (for user)
+app.use(express.urlencoded({ extended: true})); // to parse from data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
