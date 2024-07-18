c5cd9ac3da710d0205af193af2e390b52871456d
“Hugo <miseryh.pro@gmail.com>
2024-07-18 12:04:24 +0200

    1- notification routes and controllers added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,7 +13,8 @@ export const getUserProfile = async (req, res) => {
         if (!user) {
             return res.status(404).json({message: "User not found"});
         }
-        res.status(200)/json(user);
+        
+        res.status(200).json(user);
 
     } catch (error) {
         console.log("Error in getUserProfile: ", error.message);
