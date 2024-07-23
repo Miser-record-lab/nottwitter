b093170f1272b31eb956edce0227a1dde7150407
“Hugo <miseryh.pro@gmail.com>
2024-07-23 17:45:43 +0200

    3- ccreate post function added

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 8000;
 
 console.log(process.env.MONGO_URI);
 
-app.use(express.json()); // to parse req.body (for user)
+app.use(express.json({limit:"5mb"})); // to parse req.body (for user)
 app.use(express.urlencoded({ extended: true})); // to parse from data(urlencoded)
 
 app.use(cookieParser());
